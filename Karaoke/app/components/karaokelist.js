434d5ac47fa96311b973d009ae3197e9b1508d3c
CuongTT <vfa.cuongtt@gmail.com>
2016-11-30 18:30:15 +0700

    Commit loaddata custom method

--- Karaoke/app/components/karaokelist.js
+++ Karaoke/app/components/karaokelist.js
@@ -9,9 +9,12 @@ import {
 } from 'react-native';
 
 var SQLite = require('react-native-sqlite-storage');
+var GiftedListView = require('react-native-gifted-listview');
+
 var db;
 var listData = new Array();
 var ds;
+var that;
 
 class KaraokeList extends Component {
 
@@ -30,46 +33,66 @@ class KaraokeList extends Component {
   constructor(props){
     super(props);
 
-    ds = new ListView.DataSource({rowHasChanged: (r1,r2) => r1 !== r2});
+    that = this;
+
     this.state = {
       text:"",
-      isLoading: true,
-      // listItems: [],
-       dataSource: ds.cloneWithRows(listData),
+      isLoading: true
     }
 
-    
-    
   }
 
   componentDidMount() {
-    this.loadData();
+    // this.loadData();
+
   }
 
-  loadData() {
+  loadData(page = 1, callback) {
+    var limit = 15;
+    var offset = (page - 1) * limit;
+
+
     db = SQLite.openDatabase({name : 'karaoke_db.sqlite', createFromLocation : 1},this.openCB, this.errorCB);
     db.transaction((tx) => {
-      tx.executeSql('SELECT * FROM tblDanhSachBaiHat ', [] , (tx, results) => {
+      tx.executeSql('SELECT * FROM tblDanhSachBaiHat LIMIT ' + limit + ' OFFSET ' + offset, [] , (tx, results) => {
         console.log('Query completed');
+
         var len = results.rows.length;
+        var data = [];
+
         for (let i = 0; i < len; i++) {
           let row = results.rows.item(i);
           
-          listData.push(row);
+          data.push(row);
       
         }
 
+        if (callback) {
+          callback(data);
+        }
+
         this.setState({
-          dataSource: ds.cloneWithRows(listData),
-          isLoading: false,
+          // dataSource: ds.cloneWithRows(listData),
+          isLoading: false
         });
 
         // console.log('asdasdas', this.state.dataSource);
       
       });
     });
+  }
 
-    
+
+  onFetch(page = 1, callback, options) {
+    that.loadData(page, (rows) => {
+      if (rows.length == 0) {
+        callback(rows, {
+          allLoaded: true // the end of the list is reached
+        });
+      } else {
+        callback(rows);
+      }
+    });
 
   }
 
@@ -84,6 +107,29 @@ class KaraokeList extends Component {
     );
   }
 
+// Cuong BK START
+  // render() {
+  //   return (
+  //     <View style={styles.container}>
+  //       <TextInput
+  //             style={{marginTop: 55, height: 45, borderColor: 'gray', borderWidth: 6, alignSelf: 'stretch',}}
+  //             onChangeText={(text) => this.setState({text})}
+  //             value={this.state.text} />
+
+        
+  //         {this.state.isLoading ? <ActivityIndicator size='large' style={styles.container}/> :
+  //           <ListView 
+  //             style = {{marginTop: 50}}
+  //             dataSource = {this.state.dataSource}
+  //             renderRow ={this.renderRow}
+  //             initialListSize={12}
+  //           />
+
+  //         }
+  //     </View>
+  //   )
+  // }
+  // Cuong BK END
   render() {
     return (
       <View style={styles.container}>
@@ -93,15 +139,22 @@ class KaraokeList extends Component {
               value={this.state.text} />
 
         
-          {this.state.isLoading ? <ActivityIndicator size='large' style={styles.container}/> :
-            <ListView 
+          
+            <GiftedListView 
               style = {{marginTop: 50}}
-              dataSource = {this.state.dataSource}
-              renderRow ={this.renderRow}
-              initialListSize={12}
+              rowView ={this.renderRow}
+              onFetch = {this.onFetch}
+              initialListSize={15}
+              firstLoader={true} // display a loader for the first fetching
+              pagination={true} // enable infinite scrolling using touch to load more
+              refreshable={true} // enable pull-to-refresh for iOS and touch-to-refresh for Android
+              withSections={false} // enable sections
+               enableEmptySections = { true }
+               rowHasChanged={(r1,r2)=>{
+               r1.id !== r2.id
+          }}
             />
-
-          }
+          
       </View>
     )
   }
