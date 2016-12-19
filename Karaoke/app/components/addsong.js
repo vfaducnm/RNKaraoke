ce737dd829fcdc4cc8de1b7db3d298ba04284569
CuongTT <vfa.cuongtt@gmail.com>
2016-12-19 16:52:15 +0700

    Revert "validate song input"
    This reverts commit d2ce0d6c7c8dcb264935b78b9034dbefe10d8a13.

--- Karaoke/app/components/addsong.js
+++ Karaoke/app/components/addsong.js
@@ -6,177 +6,50 @@ import {
   View,
   Image,
   TextInput,
-  TouchableOpacity,
-  Alert,
-  Picker
 } from 'react-native';
+
+import { Form, InputField,
+        Separator, SwitchField, LinkField ,
+        PickerField, DatePickerField
+      } from 'react-native-form-generator';
+
 
 var wStar = require('../../image/whiteStar.png');
 var star = require('../../image/star.png');
 const stylesCSS = require('../stylesCSS.js');
-var SQLite = require('react-native-sqlite-storage');
-var db;
-var favorite = 0;
-export class InsertSong extends Component {
+
+export class AddSong extends Component {
   constructor(props){
     super(props);
-    this.state = {
-      id: '',
-      lang: 'en',
-      songName: '',
-      author: '',
-      lyric: ''
-    };
+    this.state = {text: ""};
 
-    db = SQLite.openDatabase({name : 'karaoke_db.sqlite', createFromLocation : 1},this.openCB, this.errorCB);
     console.log(this.props)
   }
 
-
-
-  insertDB(objectSong) {
-    console.log('lang',objectSong.lang);
-    console.log('id',objectSong.id);
-    console.log('songName',objectSong.songName);
-    console.log('author',objectSong.author);
-    console.log('lyric',objectSong.lyric);
-
-
-    db.transaction( (tx) => {
-        tx.executeSql(" INSERT INTO tblDanhSachBaiHat values ("
-                        + objectSong.id +','
-                        +"'"+ objectSong.songName+"'" +','
-                        +"'"+ objectSong.songName +"'" +','
-                        +"'"+ objectSong.lang +"'" + ','
-                        +"'"+ objectSong.lyric +"'" + ','
-                        +"'"+ objectSong.author +"'" + ','
-                        +"'"+ favorite +"'" +
-                        ")" , [], (tx, results) =>{
-          console.log('Query completed');
-          Alert.alert(
-                '',
-                'Insert song completed!',
-                [
-                  {text: 'OK', onPress: () => console.log('OK Pressed!')},
-                ]
-              )
-            });
-      },(err) =>{
-       console.log('transaction error: ', err.message);
-    });
-    this.setState({
-      id: '',
-      lang: 'en',
-      songName: '',
-      author: '',
-      lyric: ''
-    });
-  }
-
-  // loadImage(id) {
-  //   if (this.props.detailData.favorite) {
-  //     return star;
-  //   } else {
-  //     return wStar;
-  //   }
-  // }
-
-  checkField(objectSong){
-    if(this.state.id == ''){
-      Alert.alert(
-            'Error',
-            'Please type song id',
-            [
-              {text: 'OK', onPress: () => console.log('OK Pressed!')},
-            ]
-          )
-    } else if(this.state.lang == ''){
-      Alert.alert(
-            'Error',
-            'Please type song language',
-            [
-              {text: 'OK', onPress: () => console.log('OK Pressed!')},
-            ]
-          )
-    } else if(this.state.songName == ''){
-      Alert.alert(
-            'Error',
-            'Please type song name',
-            [
-              {text: 'OK', onPress: () => console.log('OK Pressed!')},
-            ]
-          )
-    } else {
-      this.insertDB(objectSong)
-    }
-  }
-
-  // <TextInput
-  //   style={{ marginLeft: 30,flex: 0.9,height: 30, borderColor: 'gray', borderWidth: 1}}
-  //   onChangeText={(lang) => this.setState({lang})}
-  //   value={this.state.lang}
-  // />
-
   render() {
     return (
-      <View style={stylesCSS.contentAddSong}>
-        <View style={stylesCSS.addSongView}>
-          <Text>Lang(*): </Text>
-          <Picker
-            style={{ marginLeft: 30,flex: 0.9,height: 50, borderColor: 'gray', borderWidth: 1}}
-            selectedValue={this.state.lang}
-            onValueChange={(language) => this.setState({lang: language})}>
-            <Picker.Item label="English" value="en" />
-            <Picker.Item label="Vietnamese" value="vi" />
-          </Picker>
-        </View>
-        
-        <View style={stylesCSS.addSongView}>
-          <Text>Id(*): </Text>
-          <TextInput
-            style={{ marginLeft: 30,flex: 0.9,height: 30, borderColor: 'gray', borderWidth: 1}}
-            onChangeText={(id) => this.setState({id})}
-            value={this.state.id}
-            keyboardType='numeric'
-          />
-        </View>
+      <View style={stylesCSS.addSong}>
 
-        <View style={stylesCSS.addSongView}>
-          <Text>Song name(*): </Text>
-          <TextInput
-            style={{ marginLeft: 10,flex: 0.9,height: 30, borderColor: 'gray', borderWidth: 1}}
-            onChangeText={(songName) => this.setState({songName})}
-            value={this.state.songName}
-          />
-        </View>
+        <Text style={styles.welcome}>
+          Please enter all information
+        </Text>
 
-        <View style={stylesCSS.addSongView}>
-          <Text>Author: </Text>
-          <TextInput
-            style={{ marginLeft: 30,flex: 0.9,height: 30, borderColor: 'gray', borderWidth: 1}}
-            onChangeText={(author) => this.setState({author})}
-            value={this.state.author}
-          />
-        </View>
-
-        <View style={stylesCSS.addSongView}>
-          <Text>Lyric: </Text>
-          <TextInput
-            style={{ marginLeft: 30,flex: 0.9,height: 30, borderColor: 'gray', borderWidth: 1}}
-            onChangeText={(lyric) => this.setState({lyric})}
-            value={this.state.lyric}
-          />
-        </View>
-
-        <View style={{backgroundColor:'#e5e5e5'}}>
-          <TouchableOpacity
-            onPress = {this.checkField.bind(this,this.state)}>
-            <Text> Add Song </Text>
-          </TouchableOpacity>
-        </View>
+        <Form label="Add Song">
+<InputField ref='id' label='Id:' placeholder='id'/>
+<InputField ref='title' label='Title:' placeholder='title'/>
+<InputField ref='title_simple' label='Title Simple:' placeholder='title simple' />
+<PickerField ref='lang' label='Language:' placeholder='lang'
+          options={{
+            male: 'EN',
+            female: 'VN'
+          }}/>
+<InputField ref='lyric' label='Lyric:' placeholder='lyric'/>
+<InputField ref='source' label='Source:' placeholder='source'/>
+              </Form>
 
       </View>
     );
+
   }
 }
 
@@ -214,4 +87,4 @@ const styles = StyleSheet.create({
   }
 });
 
-module.exports = InsertSong;
+module.exports = AddSong;
